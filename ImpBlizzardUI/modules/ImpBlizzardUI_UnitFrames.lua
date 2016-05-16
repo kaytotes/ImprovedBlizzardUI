9117e216673549e10b08eb3a1f99570d26ecc088
kaytotes <sbobshillito@gmail.com>
2016-05-16 21:40:31 +0100

    All of the major frames moved.

--- ImpBlizzardUI/modules/ImpBlizzardUI_UnitFrames.lua
+++ ImpBlizzardUI/modules/ImpBlizzardUI_UnitFrames.lua
@@ -1,7 +1,7 @@
 --[[
     ImpBlizzardUI/modules/ImpBlizzardUI_UnitFrames
     Handles and modifies the Blizzard unit frames
-    Current Features:
+    Current Features: Player Frame, Target Frame, Focus Frame, Party Frames, Boss Frames, Arena Frames
     Todo: All rescaling
 ]]
 local _, ImpBlizz = ...;
@@ -28,14 +28,42 @@ end
 -- Does the bulk of the tweaking to the unit frames
 local function AdjustUnitFrames()
     if(InCombatLockdown() == false) then
-        ModifyFrame(PlayerFrame, "CENTER", nil, -265, -150, 1.45); -- Player Frame
-        ModifyFrame(TargetFrame, "CENTER", nil, 265, -150, 1.45); -- Target Frame
+        ModifyFrame(PlayerFrame, "CENTER", nil, -265, -150, 1.40); -- Player Frame
+        ModifyFrame(TargetFrame, "CENTER", nil, 265, -150, 1.40); -- Target Frame
+        TargetFrame.buffsOnTop = true;
+        ModifyFrame(FocusFrame, "TOPLEFT", nil, 300, -200, 1.25); -- Focus Frame
+
+        -- Party Frames
+        ModifyFrame(PartyMemberFrame1, "LEFT", nil, 100, 125, 1.6); -- Move the first one (Others are children)
+        for i = 2, 4 do _G["PartyMemberFrame"..i]:SetScale(1.6); end -- Resize the other children
+
+        -- Boss Frames
+        for i = 1, 5 do -- Scale Them
+            _G["Boss"..i.."TargetFrame"]:SetParent(UIParent);
+            _G["Boss"..i.."TargetFrame"]:SetScale(0.95);
+            _G["Boss"..i.."TargetFrame"]:SetFrameStrata("BACKGROUND");
+        end
+        for i = 2, 5 do -- Adjust Positions
+            _G["Boss"..i.."TargetFrame"]:SetPoint("TOPLEFT", _G["Boss"..(i-1).."TargetFrame"], "BOTTOMLEFT", 0, 15);
+        end
+
+        -- Arena Frames
+        for i=1, 5 do
+            _G["ArenaPrepFrame"..i]:SetScale(1.75);
+        end
+        ArenaEnemyFrames:SetScale(1.75);
     end
 end
 
 local function HandleEvents(self, event, ...)
-    if( event == "PLAYER_ENTERING_WORLD" ) then
+    if(event == "PLAYER_ENTERING_WORLD") then
         AdjustUnitFrames();
+    end
+
+    if(event == "UNIT_EXITED_VEHICLE" or event == "UNIT_ENTERED_VEHICLE") then
+        if(UnitControllingVehicle("player") or UnitHasVehiclePlayerFrameUI("player")) then
+            SetUnitFrames();
+        end
     end
 end
 
