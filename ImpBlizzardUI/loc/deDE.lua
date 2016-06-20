e35ab9152a3ce888bfdec9f426a364b2822b1cb2
Benjamin <ben2k1690@googlemail.com>
2016-06-20 18:03:19 +0200

    Added German translation
    - Added german translation

---
+++ ImpBlizzardUI/loc/deDE.lua
@@ -0,0 +1,61 @@
+-- Possible Locales - deDE, esES, esMX, frFR, koKR, zhCN, zhTW
+
+local _, ImpBlizz = ...;
+
+if(GetLocale() == "localeHere") then
+
+    -- Micro Menu
+    ImpBlizz["Character"] = "Charakter“;
+    ImpBlizz["Spellbook"] = "Zauberbuch";
+    ImpBlizz["Talents"] = "Talente";
+    ImpBlizz["Achievements"] = "Erfolge";
+    ImpBlizz["Quest Log"] = "Quest Log";
+    ImpBlizz["Guild"] = "Gilde";
+    ImpBlizz["Group Finder"] = "Gruppen Suche";
+    ImpBlizz["Collections"] = "Sammlungen";
+    ImpBlizz["Dungeon Journal"] = "Dungeonkompendium";
+    ImpBlizz["Swap Bags"] = "Taschen";
+    ImpBlizz["ImpBlizzardUI"] = "Verbessertes Blizzard UI"
+    ImpBlizz["Log Out"] = "Ausloggen";
+    ImpBlizz["Force Exit"] = "Verlassen erzwingen";
+
+    -- Merchant
+    ImpBlizz["Items Repaired from Guild Bank"] = "Items durch die Gildenbank repariert";
+    ImpBlizz["Can not Repair from Guild Bank"] = "Kann die Gildenbank nicht zum reparieren verwenden";
+    ImpBlizz["Items Repaired from Own Money"] = "Items durch das eigene Geld repariert";
+    ImpBlizz["Sold Trash Items"] = "Verkaufter Schrott";
+
+    -- Combat
+    ImpBlizz[" killed "] = " getötet ";
+    ImpBlizz["Killing Blow!"] = "Todesstoß!";
+    ImpBlizz["HP < 50% !"] = "Gesundheit < 50% !";
+    ImpBlizz["HP < 25% !!!"] = "Gesundheit < 25% !!!";
+
+    -- Config Headers
+    ImpBlizz["Combat"] = "Kampf";
+    ImpBlizz["Miscellaneous"] = "Sonstiges";
+    ImpBlizz["User Interface"] = "Benutzeroberfläche";
+
+    -- Config Options
+    ImpBlizz["Display Class Icon"] = "Zeige Klassen Icons";
+    ImpBlizz["Display Health Warnings"] = "Zeige Gesundheits Warnungen";
+    ImpBlizz["Display PvP Kill Tracker"] = "Zeige PVP Todeszähler";
+    ImpBlizz["Display Class Colours"] = "Zeige Klassenfarbe";
+    ImpBlizz["Auto Repair"] = "Automatisch Reparieren";
+    ImpBlizz["Use Guild Bank For Repairs"] = "Benutze die Gildenbank zum reparieren";
+    ImpBlizz["Auto Sell Trash"] = "Automatisch verkaufen von Schrott";
+    ImpBlizz["AFK Mode"] = "AFK Modus";
+    ImpBlizz["Display System Statistics"] = "Zeige Systemeinstellungen";
+    ImpBlizz["Display Player Co-Ordinates"] = "Zeige Spieler Koordinaten";
+    ImpBlizz["Display Art"] = "Zeige Verziehrungen";
+    ImpBlizz["Auto-Hide Quest Tracker"] = "Verstecke den Quest-Tracker Automatisch"
+    ImpBlizz["Highlight Killing Blows"] = "Hebe Todesstöße hervor"
+    ImpBlizz["PvP"] = "PvP"
+    ImpBlizz["Minify Blizzard Strings"] = "Verkleinere Blizzard Strings"
+    ImpBlizz["Style Chat"] = "Verschönere den Chat"
+    ImpBlizz["Action Bars"] = "Aktionsleisten"
+    ImpBlizz["Casting Bar Timer"] = "Zauberleisten Zeit"
+    ImpBlizz["Out of Range Indicator"] = "Außer Reichweiten Anzeige"
+    ImpBlizz["Hide Portrait Spam"] = "Verstäcke Portrait Spam"
+
+end
