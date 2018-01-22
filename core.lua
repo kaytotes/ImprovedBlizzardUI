b3e6e6471a68e23195eee182e2af173a6d87c6b2
Kaytotes <sbobshillito@gmail.com>
2018-01-22 23:53:37 +0000

    Removed pointless variable

--- core.lua
+++ core.lua
@@ -1,7 +1,5 @@
 local _, Loc = ...;
 
 ImpFont = "Interface\\AddOns\\ImprovedBlizzardUI\\media\\impfont.ttf";
-  
-Imp_Chat = {};
 
 print("|cffffff00Improved Blizzard UI " .. GetAddOnMetadata("ImprovedBlizzardUI", "Version") .. " Initialised");
