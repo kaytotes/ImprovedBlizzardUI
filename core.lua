ef2f924a02be0973c0c863e90691378da5125f30
Kaytotes <sbobshillito@gmail.com>
2018-02-18 17:42:33 +0000

    Added Experimental Tooltips Module
    Key word is experimental! No config, everything defaults to how it will be working out the box and will require LUA changes for now.

--- core.lua
+++ core.lua
@@ -30,6 +30,20 @@ function Imp.ApplyClassColours(statusBar, unit)
 end
 
 --[[
+    Gets the Unit's class colour in RGB
+
+    @ param Unit $unit The unit we're checking
+    @ return array
+]]
+function Imp.GetClassColour(unit)
+    if (UnitClass(unit)) then
+        local _, class = UnitClass(unit);
+        local c = RAID_CLASS_COLORS[class];
+        return c;
+    end
+end
+
+--[[
     Helper function for moving a Blizzard frame that has a SetMoveable flag
 
     @ param Frame $frame The Frame we're tweaking
@@ -84,6 +98,53 @@ function Imp.DestroyFrame(frame)
         frame.RegisterAllEvents = function() end;
         frame.Show = function() end;
 	end
+end
+
+--[[
+    Converts an RGB colour to Hex - Source: WoWWiki
+
+    @ param float $r Red
+    @ param float $g Green
+    @ param float $b Blue
+    @
+    @ return void
+]]
+function Imp.RGBToHex(r, g, b)
+	if (type(r) == "table") then
+		g = r.g
+		b = r.b
+		r = r.r
+	end
+	if (not r and not g and not b) then
+		r = 255
+		g = 255
+		b = 255
+	end
+	r = r <= 255 and r >= 0 and r or 0
+	g = g <= 255 and g >= 0 and g or 0
+	b = b <= 255 and b >= 0 and b or 0
+	return string.format("%02x%02x%02x", r, g, b)
+end
+
+--[[
+    Converts an RGB Percentage to Hex - Source: WoWWiki
+
+    @ param float $r Red
+    @ param float $g Green
+    @ param float $b Blue
+    @
+    @ return void
+]]
+function Imp.RGBPercToHex(r, g, b)
+	if (type(r) == "table") then
+		g = r.g
+		b = r.b
+		r = r.r
+	end
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format("%02x%02x%02x", r*255, g*255, b*255)
 end
 
 print('|cffffff00Improved Blizzard UI ' .. GetAddOnMetadata('ImprovedBlizzardUI', 'Version') .. ' Initialised');
---
+++ modules/misc/tooltips.lua
@@ -0,0 +1,180 @@
+--[[
+    modules\frames\party.lua
+]]
+local addonName, Loc = ...;
+
+local TooltipFrame = CreateFrame('Frame', nil, UIParent);
+
+local borderColour = {
+    r = 0.8,
+    g = 0.8,
+    b = 0.8,
+};
+
+local backgroundColour = {
+    r = 0,
+    g = 0,
+    b = 0
+};
+
+local guildColour = {
+    r = 1,
+    g = 0.53,
+    b = 1
+};
+
+--	Disable fade-out effect
+GameTooltip.FadeOut = GameTooltip.Hide;
+
+local function AFKStatus(unit)
+    return UnitIsAFK(unit) and '|cffAAAAAA<AFK>|r ' or UnitIsDND(unit) and '|cffAAAAAA<DND>|r ' or '';
+end
+
+function OnTooltipSetUnit_Hook(self)
+    -- Quit if needed
+    if (true == false) then return end
+    
+	if (self:IsForbidden()) then return end
+
+    -- Get the Unit
+    local name, unit = self:GetUnit()
+	if (not unit) then
+		unit = GetMouseFocus() and GetMouseFocus():GetAttribute('unit');
+	end
+    if not unit then return end
+    
+    -- Get the info we're gonna need
+    local numLines = self:NumLines();
+    local line = 1;
+	name = GetUnitName(unit);
+	local guild, rank = GetGuildInfo(unit);
+    local race = UnitRace(unit) or '';
+    local canAttack = UnitIsEnemy('player', unit);
+    local playerFaction = UnitFactionGroup('player');
+    local faction = UnitFactionGroup(unit) or canAttack and '' or playerFaction;
+	local classification = UnitClassification(unit);
+	local creatureType = UnitCreatureType(unit);
+    local factionGroup = select(1, UnitFactionGroup(unit));
+
+    -- Unit Level
+    local level = UnitLevel(unit);
+	local levelColor = GetQuestDifficultyColor(level);
+	if (level == -1) then
+		level = '??';
+		levelColor = { r = 1, g = 0, b = 0 };
+    end
+    
+    -- Friend Status
+    local isFriend = UnitIsFriend('player', unit);
+    local friendColor = { r = 1, g = 1, b = 1 };
+    
+	if (not isFriend) then
+		friendColor = { r = 1, g = 0.15, b = 0 };
+	else
+		friendColor = { r = 0, g = 0.55, b = 1 };
+    end
+
+    if (factionGroup == 'Horde') then
+        factionColour = { r = 1, g = 0.15, b = 0 };
+    else
+        factionColour = { r = 0, g = 0.55, b = 1 };
+    end
+
+    -- Set the Background Colour
+    local backdrop = GameTooltip:GetBackdrop();
+	if backdrop.insets.left == 5 then
+		backdrop.insets.left = 3;
+		backdrop.insets.right = 3;
+		backdrop.insets.top = 3;
+		backdrop.insets.bottom = 3;
+    end
+    self:SetBackdrop(backdrop);
+    self:SetBackdropColor(backgroundColour.r, backgroundColour.g, backgroundColour.b);
+
+    -- Hostility Border
+    if (true) then
+        self:SetBackdropBorderColor(friendColor.r, friendColor.g, friendColor.b);
+    else
+        self:SetBackdropBorderColor(borderColour.r, borderColour.g, borderColour.b);
+    end
+
+    -- Build the Tooltip
+    local lines = {};
+	lines[1] = GameTooltipTextLeft1:GetText();
+
+    if (UnitIsPlayer(unit)) then
+        -- Class Coloured
+        if (true) then
+            GameTooltipTextLeft1:SetFormattedText('|cff%s%s|r %s', Imp.RGBPercToHex(Imp.GetClassColour(unit)), UnitName(unit), AFKStatus(unit));
+        else
+            GameTooltipTextLeft1:SetFormattedText('%s%s', UnitName(unit), AFKStatus(unit));
+        end
+        
+        if (guild) then
+            GameTooltipTextLeft2:SetFormattedText('|cff%s%s|r', Imp.RGBPercToHex(guildColour), guild);
+            GameTooltipTextLeft3:SetFormattedText('|cff%s%s|r |cff%s%s|r', Imp.RGBPercToHex(levelColor), level, Imp.RGBPercToHex(friendColor), race);
+        else
+            GameTooltip:AddLine('', 1, 1, 1);
+			GameTooltipTextLeft2:SetFormattedText('|cff%s%s|r |cff%s%s|r', Imp.RGBPercToHex(levelColor), level, Imp.RGBPercToHex(friendColor), race);
+        end
+
+        for i = 2, numLines do
+            local line = _G['GameTooltipTextLeft'..i];
+            if not line or not line:GetText() then break end
+
+            if (factionGroup and line:GetText():find('^'..factionGroup)) then
+                line:SetFormattedText('|cff%s%s|r', Imp.RGBPercToHex(factionColour), factionGroup);
+            end
+
+            if (line:GetText():find('^'..PVP_ENABLED)) then
+                line:SetFormattedText('|cff%s%s|r', Imp.RGBPercToHex(friendColor), PVP_ENABLED);
+            end
+        end
+    else
+        for i = 2, numLines do
+			local line = _G['GameTooltipTextLeft'..i];
+            if not line or not line:GetText() then break end
+            
+			if (level and line:GetText():find('^'..LEVEL) or (creatureType and line:GetText():find('^'..creatureType))) then
+				line:SetFormattedText('|cff%s%s %s|r |cff%s%s|r', Imp.RGBPercToHex(levelColor), level, classification, Imp.RGBPercToHex(friendColor), creatureType or 'Unknown');
+            end
+            
+            if (line:GetText():find('^'..PVP_ENABLED)) then
+                line:SetFormattedText('|cff%s%s|r', Imp.RGBPercToHex(friendColor), PVP_ENABLED);
+            end
+		end
+    end
+
+    -- Update Health Bar
+    local hp = UnitHealth(unit)
+	local max = UnitHealthMax(unit)
+	
+	GameTooltipStatusBar.unit = unit;
+	GameTooltipStatusBar:SetMinMaxValues(0, max);
+	GameTooltipStatusBar:SetValue(hp);
+	GameTooltipStatusBar:ClearAllPoints();
+    GameTooltipStatusBar:SetPoint('BOTTOMLEFT', 5, 5);
+	GameTooltipStatusBar:SetPoint('BOTTOMRIGHT', -5, 5);
+	GameTooltipStatusBar:SetHeight(5);
+    GameTooltipStatusBar:SetStatusBarTexture('Interface\\TargetingFrame\\UI-StatusBar');
+
+    if (true) then
+        Imp.ApplyClassColours(GameTooltipStatusBar, unit);
+    end
+    
+    -- Lastly, Set Fonts
+    for i = 1, 20 do
+		local line = _G['GameTooltipTextLeft'..i];
+        if not line then break end
+
+        line:SetFont(ImpFont, 13, 'OUTLINE');
+	end
+end
+
+GameTooltip:HookScript('OnTooltipSetUnit', OnTooltipSetUnit_Hook)
+
+hooksecurefunc('GameTooltip_SetDefaultAnchor', function(self, parent)
+    if (true) then
+        self:SetOwner(parent, 'ANCHOR_CURSOR');
+    end
+end)
