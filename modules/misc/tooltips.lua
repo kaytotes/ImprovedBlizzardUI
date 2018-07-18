5f82c505b8c5c2d5941b91eb6cea9c1c52b808fe
Kaytotes <kaytotes@users.noreply.github.com>
2018-07-18 22:19:15 +0100

    2.3.1 (#94)
    * Removed Dev Build Print
    
    * Added a config for hiding art
    
    * Kill Artifact Bar when leveling up.
    
    Fixes #66
    
    * A couple focus frame fixes
    
    Correctly get focus frame classification.
    Correctly display focus frame class colours.
    
    Fixes #65
    
    * Stop repeat events occuring for the kill feed
    
    Fixes #68
    
    * Tweaked position of Focus Frame and also correctly set font.
    
    Closes #69
    
    * Add cast bar timers to the target and focus cast bars (#70)
    
    * Add cast bar timers to the target and focus cast bars
    
    Added additional options to turn these off and on.
    
    * Fix indentation
    
    * Update localisation template
    
    * Removed differing classification of focus frame
    
    Also hopefully improved it recognizing changes.
    
    * Better Player frame config check
    
    * Fixed an issue with the Possesion Bar placement
    
    * Pet Name Font Change
    
    * German localisation (#77)
    
    * - added german localisation
    
    * - added new localisation to toc file
    
    * Update Changelog
    
    * Fix misprint in chat.lua
    
    * 8.0 (#91)
    
    * Swapped to my own fork of Wasabi
    
    Just in case I need to make changes for 8.0 I can PR them back to p3lim.
    
    * Bumped TOC
    
    * Revert "Swapped to my own fork of Wasabi"
    
    This reverts commit 199951099a8c04601508c784550ff3a4c0afc90a.
    
    * Move LibStub into Wasabi
    
    I have no idea why the old directory structure causes errors now but this fixes it. Feels hacky man.
    
    * Disabled modules causing eroneous errors
    
    * Disabled world map co-ords
    
    * "Fixed" the micro menu
    
    * Coords are the issue with the minimap
    
    Temporarily disabled just that bit.
    
    * Fixed coords
    
    * Gutted Action Bars Module
    
    Pretty much just moving the bag bar and the range colouring.
    
    * Couple Tweaks
    
    * Tweaked Config and Localisation
    
    * Spacing Issue
    
    * Fixed the Kill Feed
    
    * By default show target and focus cast timer
    
    * Final touches on action bars.
    
    * Smaller edit box font by default.
    
    * Tweaks to possess bar
    
    * Fix resurrect Module.
    
    * Fix misprint in chat.lua
    
    * Hide World Map Stuff in config
    
    Not sure if I'll have these done by 8.0 drop considering pre-patch patch download is starting.
    
    * Hide Chat Channel Button
    
    * Fix Killing Blow Module.
    
    * Fix Interrupt module.
    
    * This should fix the kill feed. Need to test better.
    
    * Another minor tweak.
    
    * Brute force player frame
    
    * Remove debug/
    
    * Missed another debug.
    
    * Actually print out the correct spell in Interrupts.
    
    * Disabled tooltips on the new PvP window.
    
    Guess I need to figure out what the new global is.
    
    * Stop Cast Bars Duplicating

--- modules/misc/tooltips.lua
+++ modules/misc/tooltips.lua
@@ -410,17 +410,15 @@ local function HandleEvents (self, event, ...)
 
     if (event == 'ADDON_LOADED' and ... == 'Blizzard_PVPUI') then
         fontSize = PrimaryDB.tooltipFontSize;
-        StylePvPRewardTooltip(PVPRewardTooltip);
+        --StylePvPRewardTooltip(PVPRewardTooltip);
 
-        PVPRewardTooltip:HookScript('OnShow', function ()
-            StylePvPRewardTooltip(PVPRewardTooltip);
-        end);
+        -- PVPRewardTooltip:HookScript('OnShow', function ()
+        --     StylePvPRewardTooltip(PVPRewardTooltip);
+        -- end);
 
-        ConquestTooltip:HookScript('OnShow', function ()
-            StylePvPConquestTooltip(ConquestTooltip);
-        end);
-
-        
+        -- ConquestTooltip:HookScript('OnShow', function ()
+        --     StylePvPConquestTooltip(ConquestTooltip);
+        -- end);
     end
 end
 
