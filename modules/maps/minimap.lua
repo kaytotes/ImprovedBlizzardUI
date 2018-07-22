1ada727fd53547387336fa31756aa4e68d66e9ab
Kaytotes <sbobshillito@gmail.com>
2018-07-22 01:24:33 +0100

    Fix an issue when initially zoning into some areas
    The Blizzard API sometimes just returns nil *shrug*

--- modules/maps/minimap.lua
+++ modules/maps/minimap.lua
@@ -27,12 +27,15 @@ local function CoordsFrame_Tick(self, elapsed)
 	if(CoordsFrame.elapsed >= CoordsFrame.delay) then -- Matched tick delay?
 		if(FramesDB.showMinimapCoords) then -- Update the Co-ords frame
 			-- 7.1 Restricted this so it only works in the outside world. Lame.
-			local inInstance, _ = IsInInstance();
-			if(inInstance ~= true) then
+            local map = C_Map.GetBestMapForUnit('player');
+            
+			if(map) then
 				if(Minimap:IsVisible()) then
-					local x, y = C_Map.GetPlayerMapPosition(C_Map.GetBestMapForUnit("player"), "player"):GetXY();
+					local x, y = C_Map.GetPlayerMapPosition(map, 'player'):GetXY();
 					if(x ~= 0 and y ~= 0) then
-						CoordsFrame.text:SetFormattedText('(%d:%d)', x * 100, y * 100);
+                        CoordsFrame.text:SetFormattedText('(%d:%d)', x * 100, y * 100);
+                    else
+                        CoordsFrame.text:SetText('');
 					end
                 end
             else
