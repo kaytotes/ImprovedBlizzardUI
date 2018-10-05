86a2f37ed3385f3d4c21736aeff6d134f5b48d6d
Geoff <geoffrey.p.hughes@gmail.com>
2018-10-05 13:16:53 +1000

    Possible proper Fix for #2
    Define a colour table for when `RAID_CLASS_COLORS` on rare occasion returns nil.
    V1.1.8 Changes
    
    Change log updated for v1.1.8

--- core.lua
+++ core.lua
@@ -96,11 +96,12 @@ end
     @ return array
 ]]
 function Imp.GetClassColour(unit)
-    if (UnitClass(unit)) then
-        local _, class = UnitClass(unit);
-        local c = RAID_CLASS_COLORS[class];
-        return c;
+  local _, class = UnitClass(unit)
+  if class and RAID_CLASS_COLORS[class] then
+    return RAID_CLASS_COLORS[class]
     end
+    -- Return Default Colour table if RAID_CLASS_COLORS returns nil (just in-case)
+    return { r = 1, g = 1, b = 1 }
 end
 
 --[[
