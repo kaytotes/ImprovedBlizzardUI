1bdc752e95c7b17f98fcaafca2d936c988f3336f
Kaytotes <sbobshillito@gmail.com>
2018-12-20 06:44:06 +0000

    Added some helpers.
    GetClassColour fix cherry picked from https://github.com/Misunderstood-Wookiee/ImprovedBlizzardUIPlus/commit/86a2f37ed3385f3d4c21736aeff6d134f5b48d6d

--- helpers.lua
+++ helpers.lua
@@ -31,3 +31,54 @@ function Helpers.format_num(number)
 
     return minus .. int:reverse():gsub("^,", "") .. fraction;
 end
+
+--[[
+    Gets the Unit's class colour in RGB
+    @ param Unit $unit The unit we're checking
+    @ return array
+]]
+function Helpers.GetClassColour(unit)
+    local _, class = UnitClass(unit);
+
+    -- If we can find the class then return the class colours.
+    if (class and RAID_CLASS_COLORS[class]) then
+        return RAID_CLASS_COLORS[class];
+    end
+
+    -- Under rare circumstances can be nil. Just return white.
+    return Helpers.colour_pack(1, 1, 1, 1);
+end
+
+--[[
+    Converts an RGB Percentage to Hex - Source: WoWWiki
+    @ param float $r Red
+    @ param float $g Green
+    @ param float $b Blue
+    @
+    @ return void
+]]
+function Helpers.RGBPercToHex(r, g, b)
+	if (type(r) == "table") then
+		g = r.g
+		b = r.b
+		r = r.r
+    end
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format("%02x%02x%02x", r*255, g*255, b*255)
+end
+
+--[[
+    Applies the unit's class colour to the status bar that is passed in
+    @ param Frame $statusBar The Status Bar we're tweaking
+    @ param Unit $unit The unit we're checking
+    @ return void
+]]
+function Helpers.ApplyClassColours(statusBar, unit)
+    if ( UnitIsConnected(unit) and unit == statusBar.unit and UnitClass(unit) ) then
+        local _, class = UnitClass(unit);
+        local c = RAID_CLASS_COLORS[class];
+        statusBar:SetStatusBarColor(c.r, c.g, c.b );
+    end
+end
